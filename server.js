9a4dd0502eb82358de10daf0cfd20a20fc7f2243
Gerardo Longo <gerardo.longo@entaksi.eu>
2022-08-15 23:16:45 +0200

    cors error resolved

--- server.js
+++ server.js
@@ -3,5 +3,10 @@ const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/brick-link-app'));
 app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname + '/dist/brick-link-app/index.html'));});
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.sendFile(path.join(__dirname + '/dist/brick-link-app/index.html'));
+});
 app.listen(process.env.PORT || 8080);
