305a7a597c3257b3e6c029a75951c49cfa53c772
Gerardo Longo <gerardo.longo@entaksi.eu>
2022-08-20 01:41:40 +0200

    server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-/*
+
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -7,11 +7,3 @@ app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname + '/dist/brick-link-app/index.html'));
 });
 app.listen(process.env.PORT || 8080);
- */
-
-const express = require('express');
-const app = express();
-
-app.use(express.static(__dirname + '/dist'));
-app.get('/*', (req, res) => res.sendFile(__dirname + '/dist/brick-link-app/index.html'));
-app.listen(process.env.PORT || 4200);
