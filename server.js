6d9e8c367ca06fdc1520b59990344ab393115791
Gerardo Longo <gerardo.longo@entaksi.eu>
2022-09-25 10:44:59 +0200

    Cors policy

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-
-/*
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -8,19 +6,3 @@ app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname + '/dist/brick-link-app/index.html'));
 });
 app.listen(process.env.PORT || 8080);
-*/
-
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 8080;
-const path = require('path');
-
-app.use(express.static('/dist/brick-link-ap'));
-
-app.get('*', function(req,res) {
-    res.sendFile(path.resolve('/dist/brick-link-app/index.html'));
-});
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-});
